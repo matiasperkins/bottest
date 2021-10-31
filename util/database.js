2c6b31fcd3f449b3af71fc36f49ac3f3c283b43a
matiasperkins <perkins.matias91@gmail.com>
2021-10-31 15:36:37 -0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'test',
-    database: 'bot-test',
+    database: 'bottest',
     password: '1234'
 });
 
